8b247f0ca122eadf49250f1758edd54193121858
Gilbert Makori <joshinekerubo60@gmail.com>
2022-09-07 21:16:46 -0700

    C program that prints exactly

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 int main(void)
 {
+	/**
+	 *
+	 *  * Description: main - Prints out a string to stdout.
+	 *
+	 *   * Return: 0 if success.
+	 *
+	 *    */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
