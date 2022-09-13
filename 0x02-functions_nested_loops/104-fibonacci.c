954101075e7bea4a4e8fe1f31a0e4ffff7662b41
Gilbert Makori <joshinekerubo60@gmail.com>
2022-09-13 05:14:31 -0700

    Write a program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,62 +1,39 @@
-#include "main.h"
 #include <stdio.h>
+#define LARGEST 10000000000
 
 /**
- * main - prints the first 98 fibonacci, starting with 1 and 2
- * Return: 0, and the print stated.
+ * main - main block
+ * Description: Find and print the first 98 fib numbers starting with 1 and 2.
+ * Numbers should be coma and space separated.
+ * Return: 0
  */
-
 int main(void)
 {
-	long h1 = 0, l1 = 1, h2 = 0, l2 = 1, h3 = 0, l3 = l1;
-	long max = 999999999999999999;
-	long carry = 0;
-	int i;
+	unsigned long int fr1 = 0, bk1 = 1, fr2 = 0, bk2 = 2;
+	unsigned long int hold1, hold2, hold3;
+	int count;
 
-	for (i = 0; i < 98; i++)
+	printf("%lu, %lu, ", bk1, bk2);
+	for (count = 2; count < 98; count++)
 	{
-		if (l3 > max)
+		if (bk1 + bk2 > LARGEST || fr2 > 0 || fr1 > 0)
 		{
-			carry = get_first_l(l3);
-			l3 = l3 - (carry * (max + 1));
-			l2 = l3;
-			h3 = h3 + carry;
-			h2 = h3;
+			hold1 = (bk1 + bk2) / LARGEST;
+			hold2 = (bk1 + bk2) % LARGEST;
+			hold3 = fr1 + fr2 + hold1;
+			fr1 = fr2, fr2 = hold3;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu%010lu", fr2, bk2);
 		}
-		if (h3 == 0)
-			printf("%ld", l3);
 		else
-			printf("%ld%ld", h3, l3);
-		l3 = l1 + l2;
-		l1 = l2;
-		l2 = l3;
-		h3 = h1 + h2;
-		h1 = h2;
-		h2 = h3;
-		if (i < 97)
+		{
+			hold2 = bk1 + bk2;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu", bk2);
+		}
+		if (count != 97)
 			printf(", ");
 	}
 	printf("\n");
 	return (0);
-}
-
-/**
- * get_first_l - obtains the first digit of the input number
- * @n: input number from which the first digit is obtained
- *
- * Return: the first digit
- */
-
-long get_first_l(long n)
-{
-	if (n < 10)
-	{
-		n = 0;
-	}
-	while (n >= 10)
-	{
-		n = n / 10;
-	}
-
-	return (n);
 }
