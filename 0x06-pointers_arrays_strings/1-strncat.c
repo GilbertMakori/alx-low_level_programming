e2be8ffee744f5154a104ef072cf7be620b9fe3e
Gilbert Makori <joshinekerubo60@gmail.com>
2022-09-21 09:18:25 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,14 +10,14 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	  int i, c;
+	int i, c;
 
- 	  for (i = 0; dest[i] != '\0'; i++)
-    		 ;
-	
- 	 for (c = 0; src[c] != '\0' && n > 0; c++, n--, i++)
-    	 {
-      		dest[i] = src[c];
-   	 }
- 	 return (dest);
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	for (c = 0; src[c] != '\0' && n > 0; c++, n--, i++)
+	{
+		dest[i] = src[c];
+	}
+	return (dest);
 }
